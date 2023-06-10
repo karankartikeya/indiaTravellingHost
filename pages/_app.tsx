e8d6abaabd3cca84460613fe32e67a3be32a435d
Karan Kartikeya <karankartikey72@gmail.com>
2023-06-10 13:33:43 +0530

    fix: adsense tag and ads.txt added, removed layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 import { MyUserContextProvider } from '@/utils/useUser'
-import Layout from '@/layout/Layout'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps<{
   initialSession: Session
@@ -14,18 +14,25 @@ export default function App({ Component, pageProps }: AppProps<{
   const [supabaseClient] = useState(() => createClientComponentClient())
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <MyUserContextProvider>
-        <Layout>  
+
+    <>
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7612880055830834"
+      />
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <MyUserContextProvider>
           <main>
             <Component {...pageProps} />
           </main>
-        </Layout>
-      </MyUserContextProvider>
+        </MyUserContextProvider>
 
-    </SessionContextProvider>
+      </SessionContextProvider>
+    </>
   )
 }
